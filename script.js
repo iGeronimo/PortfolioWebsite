5b03803dc6c8374b53c68439f48611d8aca9a9c9
iGeron1mo <mathijsL@hotmail.com>
2024-01-28 22:30:06 +0100

    Added IMT&S project to website

--- script.js
+++ script.js
@@ -58,5 +58,31 @@ function changeWord() {
     }, 1000); // Adjust the delay (in milliseconds) to match the transition duration
   }
 
+  const feedback = ['Mathijs was always positive, always open to feedback, as shown in the multiple team discussions that we held, and cooperative within the team. It was a pleasure working alongside him', 
+  'Very motivated team player that is willing to listen to anything the team is struggling with. Provides quality work.',
+   'Well spoken and easy to get along with. Knows when to work and when to be casual.', 
+   'Mathijs is a charismatic person who knows when to be informal but also when to put the team to work.'];
+  let currentFeedbackIndex = 0;
+
+  function changeFeedback() {
+    const currentFeedbackElement = document.getElementById('current-feedback');
+
+    currentFeedbackElement.style.opacity = 0;
+
+    // After the fading out transition ends, update the content and fade in the next word
+    setTimeout(() => {
+      // Get the next word from the array
+      currentFeedbackIndex = (currentFeedbackIndex + 1) % feedback.length;
+      const nextFeedback = feedback[currentFeedbackIndex];
+
+      // Update the content
+      currentFeedbackElement.textContent = nextFeedback;
+
+      // Start fading in the next word
+      currentFeedbackElement.style.opacity = 1;
+    }, 1000); // Adjust the delay (in milliseconds) to match the transition duration
+  }
+
   // Call the changeWord function repeatedly with a time interval
   setInterval(changeWord, 3000); // Adjust the interval (in milliseconds) between word changes
+  setInterval(changeFeedback, 6000);
